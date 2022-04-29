b631b9aa584f18dafb99fd2f8550a06e334124da
sossut <mikki.alakari@elisanet.fi>
2022-04-29 11:52:57 +0300

    nada

--- src/components/MediaRow.js
+++ src/components/MediaRow.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext} from 'react';
 import PropTypes from 'prop-types';
 import {mediaUrl} from '../utils/variables';
 import {safeParseJson} from '../utils/functions';
@@ -8,22 +8,30 @@ import {MediaContext} from '../contexts/MediaContext';
 const MediaRow = ({file}) => {
   const {genres} = useContext(MediaContext);
   console.log(genres);
+  // const genreList = genres.genres;
   const desc = safeParseJson(file.description) || {
     description: file.description,
     rating: file.rating,
     orignal_title: file.orignal_title,
     release_date: file.release_year,
+    genre_ids: file.genre_ids,
   };
   let year;
   if (desc.release_date) {
     year = desc.release_date.slice(0, 4);
   }
-  const magic = () => {
-    console.log('magic');
-  };
-  useEffect(() => {
-    magic();
-  }, []);
+  // const genreCheck = () => {
+  //   const array = [];
+  //   desc.genre_ids.forEach((item) => {
+  //     genres.genres.forEach((genre) => {
+  //       if (item === genre.id) {
+  //         array.push(genre.name);
+  //       }
+  //     });
+  //   });
+  //   return array;
+  // };
+  // console.log(genreCheck());
   return (
     <tr>
       <td>
